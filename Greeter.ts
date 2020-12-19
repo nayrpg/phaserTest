d67968ec1ac6e11741055b533aca1bd32ebc88e2
nayrpg <nayrpg@gmail.com>
2020-12-19 11:22:09 -0500

    Get Hello World Running with Phaser 3

---
+++ Greeter.ts
@@ -0,0 +1,25 @@
+class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(el: HTMLElement) {
+        this.element = el
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span')
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+    start() {
+        this.timerToken = setInterval(() => {this.span.innerText = new Date().toUTCString();}, 500);
+    }
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+}
+
+window.onload = () => {
+    var el = document.getElementById("content");
+    var greeter = new Greeter(el);
+    greeter.start();
+}
